c4b6b767a57715e8bd3281dc59ad673b10912cdb
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-04-18 17:04:33 -0500

    add albums_seeder.sql

--- albums_migration.sql
+++ albums_migration.sql
@@ -7,11 +7,26 @@ CREATE TABLE IF NOT EXISTS albums  (
                                       id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                                       artist VARCHAR(50),
                                       name VARCHAR(50) NOT NULL,
-                                      release_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+                                      release_date INT,
                                       sales FLOAT,
                                       genre VARCHAR(50),
                                       PRIMARY KEY (id)
 );
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Jay-Z', 'Reasonable Doubt', 1994, 4000000, 'Hip-Hop');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('NAS', 'Illmatic', 1994, 3000000, 'Hip-Hop');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('OutKast', 'ATLiens', 1996, 300000, 'Hip-Hop');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('A Tribe Called Quest', 'Beats, Rhymes and Life', 1996, 5000000, 'Hip-Hop');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Talib Kweli and HiTek', 'Reflection Eternal', 1998, 4000000, 'Hip-Hop');
 
 DESCRIBE albums;
 
