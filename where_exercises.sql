8d80f3b824b1a7c8a35b841e10e8ca7cd8a43213
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-04-19 15:25:07 -0500

    add where_exercises.sql

---
+++ where_exercises.sql
@@ -0,0 +1,26 @@
+USE employees;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+   AND gender = 'M';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%E' OR 'E%';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%E' AND last_name LIKE 'E%';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
