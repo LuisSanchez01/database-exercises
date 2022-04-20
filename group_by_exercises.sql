9e59735c4d9937b76c7152fd71914acb52de1373
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-04-20 12:16:27 -0500

    add group_by_exercises.sql

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,1 +1,35 @@
 USE employees;
+
+SELECT DISTINCT title
+FROM titles;
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE '%E' AND last_name LIKE 'E%'
+GROUP BY last_name;
+
+SELECT DISTINCT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%E' AND last_name LIKE 'E%'
+GROUP BY first_name, last_name
+ORDER BY COUNT(*) DESC;
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY last_name
+ORDER BY COUNT(*) DESC;
+
+SELECT DISTINCT last_name, COUNT(*)
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY last_name
+ORDER BY COUNT(*) DESC;
+
+SELECT DISTINCT gender, COUNT(*)
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender
+ORDER BY COUNT(*) DESC;
+
+
