360fc0fb9327aac9863572bdf363d3479b3ac10b
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-04-20 09:18:41 -0500

    add edit to where_exercises.sql

--- where_exercises.sql
+++ where_exercises.sql
@@ -5,6 +5,12 @@ FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
    AND gender = 'M';
 
+# SELECT *
+# FROM employees
+# WHERE (first_name = 'Irena'
+#     OR first_name = 'Vidya'
+#     OR first_name = 'Maya');
+
 SELECT *
 FROM employees
 WHERE last_name LIKE 'E%';
