3a74a12e9c5c6197e38e18613afa8c24011679bc
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-04-20 16:24:07 -0500

    add join_test_db.sql

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,1 +1,10 @@
 USE employees;
+
+SELECT hire_date, COUNT(*) AS employee_count
+FROM employees
+GROUP BY hire_date
+ORDER BY hire_date
+LIMIT 10;
+
+SELECT CONCAT(emp_no, ' - ', last_name, ' - ', first_name) AS full_name, birth_date AS DOB
+FROM employees;
