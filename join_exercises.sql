dc24b0176a4db3d9450275ff216132e61f5212b2
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-04-21 16:40:59 -0500

    add join_exercise.sql solutions

--- join_exercises.sql
+++ join_exercises.sql
@@ -8,6 +8,8 @@ FROM employees as e
               ON d.dept_no = de.dept_no
 WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
 
+# Just the department name
+SELECT d.dept_name FROM departments d;
 
 # Current manager for that department
 SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
@@ -17,6 +19,13 @@ FROM employees as e
          JOIN departments as d
               ON d.dept_no = de.dept_no
 WHERE de.to_date = '9999-01-01';
+
+# Other solution for the above
+SELECT  d.dept_name, CONCAT(e.first_name, ' ', e.last_name)
+FROM departments d
+JOIN dept_manager dm ON d.dept_no = dm.dept_no
+JOIN employees e ON e.emp_no = dm.emp_no
+WHERE to_date LIKE '9%';
 
 
 # Currently managed by women
@@ -35,7 +44,35 @@ JOIN titles t ON de.emp_no = t.emp_no
 WHERE de.to_date > NOW() AND t.to_date > NOW() AND departments.dept_name = 'Customer Service'
 GROUP BY title;
 
+# Another way to solve
+SELECT title, COUNT(title) AS TOTAL
+FROM titles t JOIN dept_emp de ON t.emp_no = de.emp_no
+    WHERE dept_no = 'd009'
+GROUP BY title;
 
+
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
+FROM employees as e
+         JOIN dept_manager as de
+              ON de.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = de.dept_no
+         JOIN salaries s ON e.emp_no = s.emp_no
+WHERE de.to_date = '9999-01-01'
+ORDER BY dept_name;
+
+# Bonus
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS Employee,
+       dept_name AS Department,
+       CONCAT(e.first_name, ' ', e.last_name) AS Manager
+   FROM employees e
+       JOIN dept_emp de ON e.emp_no = de.emp_no
+       JOIN departments d ON de.dept_no = d.dept_no
+       JOIN dept_manager dm ON d.dept_no = dm.dept_no
+       JOIN employees e2 ON dm.emp_no = e2.emp_no
+WHERE de.to_date LIKE '9%'
+    AND dm.to_date LIKE '9%'
+ORDER BY dept_name;
 
 
 
